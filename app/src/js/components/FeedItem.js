dcfc48445647a39d0cbee2b4890433a34ed838b9
Samir Prakash <samir.prakash@gmail.com>
2015-05-19 17:35:54 +0200

    removed <div>

--- app/src/js/components/FeedItem.js
+++ app/src/js/components/FeedItem.js
@@ -3,17 +3,15 @@ var React = require('react');
 var FeedItem = React.createClass({
   render: function () {
     return (
-        <div>
-          <li className="list-group-item">
-            <span className="badge">60</span>
-            <h4>Test Content 1</h4>
-            <span>Test Desc 1</span>
+        <li className="list-group-item">
+          <span className="badge">60</span>
+          <h4>Test Content 1</h4>
+          <span>Test Desc 1</span>
             <span className="pull-right">
                <button id="up" className="btn btn-xs btn-primary">&uarr;</button>
                <button id="down" className="btn btn-xs btn-primary">&darr;</button>
             </span>
-          </li>
-        </div>
+        </li>
     );
   }
 });
