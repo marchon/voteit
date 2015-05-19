0b7e436f8c3a44588edfa0046cff31328f567f75
Samir Prakash <samir.prakash@gmail.com>
2015-05-19 14:58:02 +0200

    gulpfile tasks completed

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'), // Main package for Gulp
     open = require('gulp-open'), // Gulp package to open in a browser
     browserify = require('gulp-browserify'), // Gulp package to browserify and reactify the app
     concat = require('gulp-concat'), // Gulp package to concat the components
-    post = process.env.port || 3031;
+    port = process.env.port || 3031;
 
 // Task to browserify the app
 gulp.task('browserify', function () {
