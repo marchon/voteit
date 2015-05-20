f83810f521e2f78b9b3ba56c9934e556d6d36830
Samir Prakash <samir.prakash@gmail.com>
2015-05-20 21:06:15 +0200

    added voteCount

--- app/src/js/components/FeedItem.js
+++ app/src/js/components/FeedItem.js
@@ -4,7 +4,7 @@ var FeedItem = React.createClass({
   render: function () {
     return (
         <li className="list-group-item">
-          <span className="badge badge-success">60</span>
+          <span className="badge badge-success">{this.props.voteCount}</span>
           <h4>{this.props.title}</h4>
           <span>{this.props.description}</span>
             <span className="pull-right">
