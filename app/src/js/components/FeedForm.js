95a655497d2c88e34cea0211f261ccb339108157
Samir Prakash <samir.prakash@gmail.com>
2015-05-20 21:17:45 +0200

    input saved from form to list

--- app/src/js/components/FeedForm.js
+++ app/src/js/components/FeedForm.js
@@ -1,16 +1,31 @@
 var React = require('react');
 
 var FeedForm = React.createClass({
+
+  handleForm: function(e) {
+    e.preventDefault();
+
+    var newItem = {
+      title: this.refs.title.getDOMNode().value,
+      description: this.refs.desc.getDOMNode().value,
+      voteCount: 0
+    }
+
+    this.refs.feedForm.getDOMNode().reset();
+
+    this.props.onNewitem(newItem);
+  },
+
   render: function () {
     var formStyles = {
       display: this.props.formDisplayed ? 'block' : 'none'
     };
 
     return (
-        <form style={formStyles} id="feedForm" className="container">
+        <form ref="feedForm" style={formStyles} id="feedForm" className="container" onSubmit={this.handleForm}>
           <div className="form-group">
-            <input type="text" className="form-control" placeholder="Title"/>
-            <input type="text" className="form-control" placeholder="Description"/>
+            <input ref="title" type="text" className="form-control" placeholder="Title"/>
+            <input ref="desc" type="text" className="form-control" placeholder="Description"/>
             <button className="btn btn-primary btn-block">Add</button>
           </div>
         </form>
