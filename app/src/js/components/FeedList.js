1bc013a8ca91c08b1ac64350f0b1234b7dc80eb5
Samir Prakash <samir.prakash@gmail.com>
2015-05-21 22:04:46 +0200

    added thins binding

--- app/src/js/components/FeedList.js
+++ app/src/js/components/FeedList.js
@@ -5,7 +5,7 @@ var FeedList = React.createClass({
   render: function () {
     var feedItems = this.props.items.map(function (item) {
       return <FeedItem title={item.title} description={item.description} voteCount={item.voteCount} onVote={this.props.onVote}/>
-    });
+    }.bind(this));
 
     return (
         <ul className="list-group container">
