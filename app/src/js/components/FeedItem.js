29a81c794dff28e5174cc646453a2ec892189260
Samir Prakash <samir.prakash@gmail.com>
2015-05-21 22:07:08 +0200

    wired up events for voting

--- app/src/js/components/FeedItem.js
+++ app/src/js/components/FeedItem.js
@@ -1,6 +1,27 @@
 var React = require('react');
 
 var FeedItem = React.createClass({
+
+  vote: function (count) {
+    this.props.onVote({
+      title: this.props.title,
+      description: this.props.description,
+      voteCount: count
+    });
+  },
+
+  voteUp: function() {
+    var count = this.props.voteCount;
+    count = count + 1;
+    this.vote(count);
+  },
+
+  voteDown: function() {
+    var count = this.props.voteCount;
+    count = count - 1;
+    this.vote(count);
+  },
+
   render: function () {
     return (
         <li className="list-group-item">
@@ -8,8 +29,8 @@ var FeedItem = React.createClass({
           <h4>{this.props.title}</h4>
           <span>{this.props.description}</span>
             <span className="pull-right">
-               <button id="up" className="btn btn-xs btn-primary">&uarr;</button>
-               <button id="down" className="btn btn-xs btn-primary">&darr;</button>
+               <button id="up" className="btn btn-xs btn-primary" onClick={this.voteUp}>&uarr;</button>
+               <button id="down" className="btn btn-xs btn-primary" onClick={this.voteDown}>&darr;</button>
             </span>
         </li>
     );
